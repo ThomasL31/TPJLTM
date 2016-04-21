4b09c405c25186a135c58a957b05413e477cb07c
ThomasL31 <thomas.legrand80670@gmail.com>
2016-04-21 09:45:04 +0200

    Create ParkingContact.java

---
+++ PROJET JAVA LTM/src/ParkingContact.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+//Dernière modification : 17/04/2016 (Thomas)
+
+public class ParkingContact extends Parking
+{
+    private static ArrayList<ParkingContact> lesParkingsContact = new ArrayList<lesParkingsContact>();
+    
+    public ParkingContact(String idP, Zone sect)
+    {
+        super(idP, sect);
+        lesParkingsContact.add(this);
+    }    
+    
+    public Parking getLibre()
+    {
+      
+    }
+    
+}
